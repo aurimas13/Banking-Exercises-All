719817379828321487fd97ded2baadf561228f1e
Aurimas Aleksandras <aurimasaleksandras@Aurimass-MacBook-Pro.local>
2024-08-30 12:41:22 +0300

    Added file

--- Me/Practical_Task_1/Correct_Ones/calculate_monthly_payment.py
+++
@@ -1,9 +0,0 @@
-def calculate_monthly_payment(principal, annual_rate, years):
-    monthly_rate = (annual_rate / 100) / 12
-    num_payments = years * 12
-    payment = principal * monthly_rate / (1 - (1 + monthly_rate) ** -num_payments)
-    print("Monthly Payment: ", payment)
-    return payment * num_payments
-
-total_payment = calculate_monthly_payment(10000, 5, 2)
-print("Total Payment: ", total_payment)
